0644582a8e0f9a8783146f3cc866ffcefbf981fc
Arkanoid87 <giacomo.cappellini.87@gmail.com>
2022-01-19 07:29:35 +0100

    got first numerical data out of ArrowArray updating readme

--- src/freccia/types.nim
+++ src/freccia/types.nim
@@ -7,7 +7,7 @@ type
   ArrowType* {.size: sizeof(int32).} = enum
     atInvalid = "invalid"
     atNull = "null"
-    atBoolean = "boolean"
+    atBoolean = "bool"
     atInt8 = "int8"
     atUInt8 = "uint8"
     atInt16 = "int16"
@@ -61,6 +61,7 @@ type
   ArrowBaseStructure = ArrowSchema | ArrowArray
 
 
+# Compile time goodies
 
 const 
   formatTypeMap* = block:
@@ -87,12 +88,51 @@ const
     res['w'] = atInvalid
     res['t'] = atInvalid
     res
+  formatTypeMapInv* = block:
+    var res: array[ArrowType, char]
+    for key, val in formatTypeMap:
+      if val != atInvalid:
+        res[val] = key
+    res
   formatChars* = block:
     var res: set[char]
     for key, val in formatTypeMap:
       if val != atInvalid:
         res.incl key
     res
+
+template dtype*(at: ArrowType): typedesc =
+  when at == atInvalid: {.error.}
+  elif at == atNull: void
+  elif at == atBoolean: bool
+  elif at == atInt8: int8
+  elif at == atUInt8: uint8
+  elif at == atInt16: int16
+  elif at == atUInt16: uint16
+  elif at == atInt32: int32
+  elif at == atUInt32: uint32
+  elif at == atInt64: int64
+  elif at == atUInt64: uint64
+  elif at == atFloat32: float32
+  elif at == atFloat64: float64
+  # TODO
+  elif at == atBinary: {.error.}
+  elif at == atLargeBinary: {.error.}
+  elif at == atUTF8String: {.error.}
+  elif at == atLargeUtf8String: {.error.}
+  else: {.error.}
+
+template children*(abs: ArrowBaseStructure): openArray[ptr ArrowBaseStructure] =
+  abs.children.toOpenArray(0, abs.nChildren.int-1)
+
+template buffers*(arr: ArrowArray): openArray[pointer] =
+  arr.buffers.toOpenArray(0, arr.nBuffers.int-1)
+
+template values*[T](arr: ArrowArray): openArray[T] =
+  let 
+    validityBitmap = arr.buffers[0]
+    values = cast[ptr UncheckedArray[T]](arr.buffers[1])
+  values.toOpenArray(0, arr.length.int-1)
 
 
 # Getters
@@ -106,68 +146,35 @@ func length*(arr: ArrowArray): int64 =
 func nullCount*(arr: ArrowArray): int64 =
   arr.nullCount
 
-func datatype*(sch: ArrowSchema): ArrowType =
+func parseType*(sch: ArrowSchema): ArrowType =
   let fchar = sch.format[0]
   if fchar in formatChars:
     formatTypeMap[fchar]
   else:
     atInvalid
 
-template children*(abs: ArrowBaseStructure): openArray[ptr ArrowBaseStructure] =
-  abs.children.toOpenArray(0, abs.nChildren.int-1)
-
-template buffers*(arr: ArrowArray): openArray[pointer] =
-  arr.buffers.toOpenArray(0, arr.nBuffers.int-1)
 
 func size*(at: ArrowType): int =
   template err = raise newException(ValueError, &"Invalid type {$at}")
   case at:
   of atInvalid: err()
-  of atNull: void.sizeof
-  of atBoolean: bool.sizeof
-  of atInt8: int8.sizeof
-  of atUInt8: uint8.sizeof
-  of atInt16: int16.sizeof
-  of atUInt16: uint16.sizeof
-  of atInt32: int32.sizeof
-  of atUInt32: uint32.sizeof
-  of atInt64: int64.sizeof
-  of atUInt64: uint64.sizeof
-  of atFloat32: float32.sizeof
-  of atFloat64: float64.sizeof
+  of atNull: atNull.dtype.sizeof
+  of atBoolean: atBoolean.dtype.sizeof
+  of atInt8: atInt8.dtype.sizeof
+  of atUInt8: atUInt8.dtype.sizeof
+  of atInt16: atInt16.dtype.sizeof
+  of atUInt16: atUInt16.dtype.sizeof
+  of atInt32: atInt32.dtype.sizeof
+  of atUInt32: atUInt32.dtype.sizeof
+  of atInt64: atInt64.dtype.sizeof
+  of atUInt64: atUInt64.dtype.sizeof
+  of atFloat32: atFloat32.dtype.sizeof
+  of atFloat64: atFloat64.dtype.sizeof
   # TODO
   of atBinary: err()
   of atLargeBinary: err()
   of atUTF8String: err()
   of atLargeUtf8String: err()
-
-
-# func values*(arr: ArrowArray, at: ArrowType): int =
-#   template err = raise newException(ValueError, &"Invalid type {$at}")
-#   case at:
-#   of atInvalid: err()
-#   of atNull: err()
-#   of atBoolean: err()
-#   of atInt8: err()
-#   of atUInt8: err()
-#   of atInt16: err()
-#   of atUInt16: err()
-#   of atInt32: err()
-#   of atUInt32: err()
-#   of atInt64: err()
-#   of atUInt64: err()
-#   of atFloat32: err()
-#   of atFloat64: err()
-#   # TODO
-#   of atBinary: err()
-#   of atLargeBinary: err()
-#   of atUTF8String: err()
-#   of atLargeUtf8String: err()
-
-#template values*(arr: ArrowArray): untyped =
-#  let 
-#    validityBitmap = arr.buffers[0]
-#    values = cast[UncheckedArray[]](arr.buffers[1])
 
 
 # Memory management
@@ -183,7 +190,7 @@ func `$`*(abs: ArrowBaseStructure): string =
   result &= $abs.type & "\n"
   
   when abs is ArrowSchema:
-    result &= &"format: {abs.format} ({abs.datatype})\n"
+    result &= &"format: {abs.format} ({abs.parseType})\n"
     result &= &"name: {abs.name}\n"
     result &= &"metadata.isNil: {abs.metadata.isNil}\n"
     result &= &"flags: {abs.flags}\n"
